56966cd41e8421afec97db19bb65ffd483e1cb82
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-06-21 13:47:10 +0300

    Implemented connection to database and implemented funcs that open database (openDB) and connected to database (connectToDB)

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"github.com/olivo4ka37/myMicroService/authentication-service/data"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+
+var counts int
 
 type Config struct {
 	DB     *sql.DB
@@ -18,10 +26,17 @@ type Config struct {
 func main() {
 	log.Println("Starting auth service")
 
-	// TODO Connect to DB
+	// Connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
 
 	// Set up config
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -34,4 +49,43 @@ func main() {
 		log.Panic(err)
 	}
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	// Вместо err можно использовать nil
+	return db, err
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("Connected to postgres YOPTA!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off (Чилим) 2 seconds")
+		time.Sleep(time.Second * 2)
+		continue
+	}
 }
