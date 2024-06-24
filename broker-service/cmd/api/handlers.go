5792298c04355e7599ea497235e47cefd350eda1
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-06-24 17:36:52 +0300

    Added two structs: RequestPayLoad and AuthPayLoad And added two functions 1. HandleSubmission //handle data for selected action 2. authenticate //Execute the authenticate request to the authenticate-service and check for status code.

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 )
+
+type RequestPayLoad struct {
+	Action string      `json:"action"`
+	Auth   AuthPayLoad `json:"auth,omitempty"`
+}
+
+type AuthPayLoad struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -15,4 +28,73 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(fmt.Errorf("Error while read json in broker-service/cmd/api/handlers.go ", err))
 	}
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayLoad RequestPayLoad
+	err := app.readJSON(w, r, &requestPayLoad)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	switch requestPayLoad.Action {
+	case "auth":
+		app.authenticate(w, requestPayLoad.Auth)
+
+	default:
+		app.errorJSON(w, errors.New("unknow action"))
+
+	}
+
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayLoad) {
+	// Создаем json файл который мы отправим в authenticate микросервис
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+
+	// Вызываем микросервис
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Убедиться в успешном отправлении запроса (получить status code)
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credential"))
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service"))
+		return
+	}
+
+	// Переменная из которой будем читать тело запроса
+	var jsonFromService jsonResponse
+
+	// Декодирование JSON из authentication-service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Authenticated!"
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
