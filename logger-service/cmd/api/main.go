96636bfe244642aae2476d5d67fa2a88d7c2e1d3
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-06-24 18:04:46 +0300

    Implement simple connect to mongoDB

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to Mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+}
+
+// connectToMongo Connects to mongo using const URL and Username and Password that holds inside the func
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting to mongoDB:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
